997bc6e2ba8cda1387f88053b894a951f2c31099
albanokainer <albanokainer@hotmail.com>
2022-09-08 22:11:37 -0300

    first commit

---
+++ clase-2/index.js
@@ -0,0 +1,80 @@
+
+let numero = 1; // Este valor es numero y el operador estricto lo tiene en cuenta
+let numeroFalso = '1' // Este valor es string y el operador estricto lo tiene en cuenta
+
+
+// if(numero <= numeroFalso){
+// 	console.log('Menor o igual');
+// }
+// else{
+// 	console.log('Mayor');
+// }
+
+// anidacion de condiciones
+// numero menor o igual a numero falo y numero es igual estrictamente igual a numeroFalso
+//  TRUE                            FALSE                   TRUE
+// if((numero <= numeroFalso) && (numero === numeroFalso) && true){
+// 	console.log('Menor o igual e iguales');
+// }
+// else{
+// 	console.log('Mayor o distintos tipos de dato');
+// }
+// No le importa si hay algun FALSE, busca y siemrpe toma al valor TRUE
+// if((numero <= numeroFalso) || (numero === numeroFalso)){
+// 	console.log('Menor o igual e iguales');
+// }
+// else{
+// 	console.log('Mayor o distintos tipos de dato');
+// }
+
+// Guardo el valor en una variable
+// let verdadero_o_falso = numero === numeroFalso;
+
+// Operador de negacion, pasa un valor a su contraparte o valor contrario
+// !TRUE  === FALSE
+// if(!(numero === numeroFalso)){
+// 	console.log('Iguales');
+// }
+// else{
+// 	console.log('Distintos tipos de dato');
+// }
+// If con anidacion de else if
+// let nombre = 'robert'
+//  if(numero== 'camila'){
+
+//  }else if(nombre === 'albno')
+// {
+
+// }else if(nombre == 'albano')
+// {
+
+// }else if(nombre == 'roberto')
+// {
+
+// }else{
+// 	console.log('No se cumplio ninguna');
+// }
+// numero vale 1, esta declarado en la linea 1
+
+// estructura switch para evaluar condiciones
+// switch (numero) {
+// 	case 33:
+// 		console.log('numero vale igual que 5');
+// 		break;
+// 	case 6:
+// 		console.log('numero vale igual que 6');
+// 	break;
+
+// 	default:
+
+// 		console.log('numero no coincide con ninguno');
+// 		break;
+// }
+
+// case tiene el valor a evaluar, de la condicion que recibe el switch
+// if (condicion) puede evaluar varias condicones, el case no
+
+
+
+
+
