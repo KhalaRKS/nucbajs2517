ad5aeb8721040b5dcf6f4441d0011a226cd5e944
“AlbaKainer” <akainer@asap-consulting.net>
2022-09-13 22:58:15 -0300

    clase 3

---
+++ clase-3/index.js
@@ -0,0 +1,60 @@
+// Declaracion de una funcion
+// Animal es la referencia al argumento que le enviamos en la invocacion
+ function mostrarNombre(nombre) {
+    console.log('Hola ' + nombre);
+ }
+
+
+// let miMascota = 'Perro'
+
+// invocamos la funcion
+
+// index++ es == lo de abajo
+// let numero = 0
+//  numero = numero + 1
+
+// console.log(numero);
+
+// 1 El inicio, let index = 0
+// 2 Una condicion, index < 10
+// 3 Una accion sobre la variable de inicio, index++
+
+
+// for (let index = 0; index <= 2; index++) {
+//     console.log('Persona numero ' + (index + 1) );
+//     let nombreQueIngresaLaPersona = prompt('Ingresar un nombre');
+//     mostrarNombre(nombreQueIngresaLaPersona)
+// }
+
+// let personas = ['Santiago', 'Juan', 'David', 'Angel', 'Franco', 'Camila'];
+
+
+// for (let index = 0; index < personas.length; index++) {
+//     // personas[index] == 'Santiago'
+//     // 'Santiago'.length - 1 == 7
+//     // 'Santiago'[7] == 'o'
+
+//     if(personas[index][personas[index].length-1] == 'l'){
+//         break;
+//     }
+//     console.log('Hola ' +  personas[index]);
+// }
+
+// let edad = 15
+
+// Ejecuta al menos 1 vez el bucle
+// do{
+//     console.log('Entran al boliche');
+
+// }while(edad > 20)
+
+// Evalua la condicion y ejecuta mientras la condiencion sea true
+// while (edad > 18 || nombre == 'pepito') {
+//     console.log('Entran al boliche');
+//     edad++
+// }
+
+let numero = 30
+
+let miSegundoArray = ['Nucba']
+let miArray = [1,2,3,4,10]
