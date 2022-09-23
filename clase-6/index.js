014803a38c61ed0b000984af06622c28b8c46802
“AlbaKainer” <akainer@asap-consulting.net>
2022-09-22 21:46:11 -0300

    clase 6

---
+++ clase-6/index.js
@@ -0,0 +1,131 @@
+
+// const persona = {
+//     nombre: 'Albano',
+//     apellido: 'Kainer',
+//     dni: 42100444
+// };
+
+
+
+// function crearPersona(nombre,apellido) {
+//     return {
+//         nombre: nombre,
+//         apellido: apellido
+//     }
+// }
+
+
+// let newPersona= crearPersona('Naiara', 'Cerrizuela')
+// let newPersona2 = crearPersona('pepe','Dominguez')
+
+// class Pizza{
+//     constructor(tipo, precio){
+//         this.tipo = tipo,
+//         this.precio = precio,
+//         this.oregano = false
+//     }
+// }
+
+// const pizza = new Pizza('molde', 1500);
+
+// pizza.oregano = false
+
+// console.log(typeof newPersona, typeof pizza);
+
+// Mutar el objeto / no recomendado
+// newPersona.edad = 22
+
+// newPersona.nombre = 'Pepito'
+
+
+// const persona = {
+//     nombre: 'Albano',
+//     apellido: 'Kainer',
+//     comentario:'Que lindo dia verdad?',
+//     extendendido: {
+//         nombre: 'pepito'
+//     },
+//     frutas: ['manzana','limon']
+// }
+
+
+//  const newPersona = {...persona, nombre:'pepito'}
+//  persona.frutas
+//  console.log(newPersona);
+
+
+// let numeros = [1,2,3,4]
+// let numeros2 = [4,5,6]
+
+// let numeros3 = [...numeros,...numeros2]
+
+// console.log(numeros3);
+
+// let nombre = '5555'
+
+// console.log(...nombre);
+
+
+
+// Destructuracao
+
+// let array = [1,2,3,4,5];
+
+// let [primerValor, segundoValor,...rest] = array
+
+// console.log(primerValor,segundoValor, rest, array);
+
+
+// let objeto = {
+//     nombre: 'Albano',
+//     apelido: 'Juarez',
+//     asd: [1,2,3],
+// }
+
+// let {asd} = objeto;
+
+// console.log(asd);
+
+// function muchosValores(primerValor,segundoValor) {
+    
+//     console.log(primerValor, segundoValor);
+// }
+
+// muchosValores(1,2,3,4,5,6,7,8);
+
+// Algunos metodos que vienen en js
+// let nombre = 'Albano'
+
+// let numero = 10
+
+// let objeto = {}
+// let array = [1,2,3,4]
+
+// let oracion = 'Me gusta jugar con la pelota'
+
+// console.log(typeof nombre);
+// console.log(typeof numero);
+// console.log(typeof objeto);
+
+// console.log(array[array.length-1]);
+
+// let nuevoNombre = nombre.toLowerCase().split('a')
+
+// console.log(nombre.trim(), nombre);
+
+// let nuevaOracion = oracion.replace('pelota', 'botella')
+
+// console.log(nuevaOracion);
+
+
+
+
+
+
+
+
+
+
+
+
+
